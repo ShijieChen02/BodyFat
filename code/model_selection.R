f54bfe73c04add3149fc49fb5afea6b23a35f772
ShijieChen02 <147095965+ShijieChen02@users.noreply.github.com>
2023-10-14 01:09:02 -0500

    Update model_selection.R

--- code/model_selection.R
+++ code/model_selection.R
@@ -5,6 +5,8 @@ lmfit <- lm(BODYFAT~AGE+WEIGHT+HEIGHT+ADIPOSITY+NECK+CHEST+ABDOMEN+WRIST,data=bf
 all_posible = ols_step_all_possible(lmfit)
 
 # other criteria like AIC perfoms worse than adjr
-top10 = all_posible[order(all_posible$adjr,decreasing = TRUE),][1:10,]
+odered_possible = all_possible[order(all_possible$adjr,decreasing = TRUE),]
 
-# choose a suitable one from top10
+# choose the model whose adjr is the greatesr while numver of predicotrs <=5.
+odered_possible = odered_possible[odered_posible$n<=5,]
+write.table(odered_possible,"odered_possible.csv",row.names=FALSE,col.names=TRUE,sep=",")
